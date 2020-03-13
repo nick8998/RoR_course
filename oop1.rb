165c47deda8937ab3608541bb17a42e33f4312c3
Nick <19nipivanov98@gmail.com>
2020-03-13 13:31:56 +0300

    again

--- oop1.rb
+++ oop1.rb
@@ -7,15 +7,11 @@ class Station
   end
 
   def add_train(train)
-    @trains << train
+    @trains << train if train.station == train.route.stations[0] or nil
   end
 
-  def list_passenger
-    @trains.select {|train| train.type == "passenger"}.each {|train| puts train.number}
-  end
-
-  def list_freight
-    @trains.select {|train| train.type == "freight"}.each {|train| puts train.number}
+  def list_by_type(type)
+    @trains.select { |train| train.type == type }
   end
 
   def send_train(train)
@@ -26,18 +22,19 @@ end
 
 class Route
   attr_reader :stations
+
   def initialize(first_station, last_station)
-    @middle_station_index = 0
     @stations = [first_station, last_station]
   end 
 
   def add_station(station)
-    @middle_station_index += 1
-    @stations.insert(@middle_station_index, station)    
+    @stations.insert(-2, station)    
   end
 
   def remove_station(station)
-    (@stations[0] == station or @stations[-1] == station ) ? puts("Error") : @stations.delete(station)
+    first_station = @stations[0]
+    last_station = @stations[-1]
+    [first_station, last_station].include?(station) ? nil : @stations.delete(station) 
   end
 
   def show_stations
@@ -46,7 +43,7 @@ class Route
 end
 
 class Train
-  attr_reader :carriages, :type, :speed
+  attr_reader :carriages, :type, :route
 
   def initialize(number, type, carriages)
     @number = number
@@ -55,50 +52,57 @@ class Train
     @speed = 0
   end
 
+  def speed
+    @speed
+  end
+
   def increase_speed(value)
     @speed += value
   end
+
   def decrease_speed(value)
     @speed <= value ? @speed = 0 : @speed -= value
   end
 
   def add_carriage
-    @speed == 0 ? @carriages += 1 : puts("Сбросьте скорость до 0")
+    @speed == 0 ? @carriages += 1 : nil
   end
+
   def remove_carriage
-    @speed == 0 ? @carriages -= 1 : puts("Сбросьте скорость до 0")
+    @speed == 0 ? @carriages -= 1 : nil
   end
 
   def add_route(route)
     @route = route
     @current_station_index = 0
+    @route.stations[@current_station_index].add_train(self)
   end
 
   def station
     @route.stations[@current_station_index]
   end
+
   def next_station
     @route.stations[@current_station_index+1]
   end
-  def last_station
+
+  def previous_station
     @route.stations[@current_station_index-1]
   end
 
   def drive_forward
-    if @current_station_index == @route.stations.size-1
-      @current_station_index = -1
-      @station = @route.stations[@current_station_index+=1]
-    else
-      @station = @route.stations[@current_station_index+=1]
+    if next_station
+      @route.stations[@current_station_index].send_train(self)
+      @current_station_index+=1
+      @route.stations[@current_station_index].add_train(self)
     end
   end
 
   def drive_back
-    if @current_station_index == 0
-      @current_station_index = @route.stations.size
-      @station = @route.stations[@current_station_index-=1]
-    else
-      @station = @route.stations[@current_station_index-=1]
+    if previous_station
+      @route.stations[@current_station_index].send_train(self)
+      @current_station_index-=1
+      @route.stations[@current_station_index].add_train(self)
     end
   end
 end
